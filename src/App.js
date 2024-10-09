6081d59ed53097f2f39e9de673d2774d8d4b265c
lordkafka456 <kevinkien47@gmail.com>
2024-10-09 10:15:38 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { formatTime } from "./formatTime";
+import useTimer from "./useTimer";
 import './App.css';
-
 function App() {
+  const { time, startTimer, stopTimer, resetTimer, active } = useTimer(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <h1>Coder Timer</h1>
+      <div className="timer__wrapper">
+        <div className="timer__display">
+          <p>{formatTime(time)}</p>
+        </div>
+        <div className="button__wrapper">
+          <button className="button" onClick={stopTimer}>
+            Stop
+          </button>
+          <button className="button" ref={active} onClick={startTimer}>
+            Start
+          </button>
+          <button className="button" onClick={resetTimer}>
+            Reset
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
